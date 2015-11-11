c6059ed3b480850372cff69fcc4e9639d79a0b62
RoyGBivDash <ks33@hawaii.edu>
2015-11-11 12:58:05 -1000

    Formatted answer so it's right aligned.

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -2,16 +2,15 @@
 
 def fizz_buzz(number):
     """Return a string representing the number, or 'fizz'/'buzz'/'fizzbuzz'"""
+    answer = ''         # So the if statesments share this instead of having their own variable
     if (number % 3) == 0 and (number % 2)== 0:
         answer = 'fizzbuzz'
     elif (number % 3) == 0:
         answer = 'fizz'
     elif (number % 2) == 0:
         answer = 'buzz'
-    else:
-        answer = number
-
-    return(answer)
+    
+    return('{:02d}{:>20}'.format(number, answer))
 
 def main():
     """Print sample output of fizz_buzz function"""
